96d42be28376131be8b01fa647fb37a2c23a8243
shadow-dragon <mallyamayur@yahoo.in>
2017-07-10 13:39:19 +0530

    Add files via upload

---
+++ week1-007.Multiplication/src/Multiplication.java
@@ -0,0 +1,13 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int a = 277;
+        int b = 111;
+        int result = a * b;
+
+        String toPrint = a + " * " + b + " = " + result;
+        System.out.println(toPrint);
+    }
+
+}
