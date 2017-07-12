348d7e1f612a6693d1dcbddfc3efad52fb6e56fe
shadow-dragon <mallyamayur@yahoo.in>
2017-07-12 19:58:15 +0530

    Add files via upload

---
+++ week1-017.GreaterNumber/src/GreaterNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GreaterNumber {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Type the first number: ");
+        int num1 = Integer.parseInt(reader.nextLine());
+        
+        System.out.println("Type the second number: ");
+        int num2 = Integer.parseInt(reader.nextLine());
+        
+        if (num1 > num2) {
+            System.out.println("Greater number: " + num1);
+        }
+        else if (num1 == num2) {
+            System.out.println("The numbers are equal!");
+        }
+        else {
+            System.out.println("Greater number: " + num2);
+        }
+    }
+}
