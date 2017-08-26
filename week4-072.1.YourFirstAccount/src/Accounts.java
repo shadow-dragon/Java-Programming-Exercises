a2c4ff0ec63e368cf846dbabfa6b9a61e88eddda
Mayur Mallya <mallyamayur@yahoo.in>
2017-08-26 14:40:18 +0530

    Add files via upload

---
+++ week4-072.1.YourFirstAccount/src/Accounts.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Accounts {
+
+    public static void main(String[] args) {
+        // Code in Account.Java should not be touched!
+        // write your code here
+        Account account = new Account("Account",100.00);
+        account.deposit(20);
+        System.out.println(account);
+    }
+
+}
